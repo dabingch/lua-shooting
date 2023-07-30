d0499aedb68c8aeff51b5dff218bc97a8b6366f4
Hao Chen <dabingchhh@gmail.com>
2023-07-30 20:45:04 +0800

    Added game state

--- main.lua
+++ main.lua
@@ -1,11 +1,14 @@
 function love.load()
+  love.mouse.setVisible(false)
+
   target = {}
   target.x = 300
   target.y = 300
   target.radius = 50
 
   score = 0
-  timer = 3
+  timer = 0
+  gameState = 1
 
   gameFont = love.graphics.newFont(40)
 
@@ -13,8 +16,6 @@ function love.load()
   sprites.sky = love.graphics.newImage('sprites/sky.png')
   sprites.target = love.graphics.newImage('sprites/target.png')
   sprites.crosshairs = love.graphics.newImage('sprites/crosshairs.png')
-
-  love.mouse.setVisible(false)
 end
 
 function love.update(dt)
@@ -24,24 +25,27 @@ function love.update(dt)
   
   if timer < 0 then
     timer = 0
+    gameState = 1
   end
 end
 
 function love.draw()
   love.graphics.draw(sprites.sky, 0, 0)
-  
+
   love.graphics.setColor(1, 1, 1)
   love.graphics.setFont(gameFont)
   
   love.graphics.print(score, 0, 0)
   love.graphics.print(math.ceil(timer), 300, 0)
   
-  love.graphics.draw(sprites.target, target.x - target.radius, target.y - target.radius)
+  if gameState == 2 then
+    love.graphics.draw(sprites.target, target.x - target.radius, target.y - target.radius)
+  end
   love.graphics.draw(sprites.crosshairs, love.mouse.getX() - 20, love.mouse.getY() - 20)
 end
 
 function love.mousepressed(x, y, button, istouch, presses)
-  if button == 1 then
+  if button == 1 and gameState == 2 then
     local mouseToTarget = distanceBetween(x, y, target.x, target.y)
 
     if mouseToTarget < target.radius then
@@ -49,6 +53,10 @@ function love.mousepressed(x, y, button, istouch, presses)
       target.x = math.random(target.radius, love.graphics.getWidth() - target.radius)
       target.y = math.random(target.radius, love.graphics.getHeight() - target.radius)
     end
+  elseif button == 1 and gameState == 1 then
+    gameState = 2   
+    timer = 10
+    score = 0
   end
 end
 
